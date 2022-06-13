55d2f45c3adaaf6699b0774975e914b536878555
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-13 18:54:24 +0200

    Removed App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,24 +24,13 @@ class DrinkTracker extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.yellow,
       ),
-      home: const DrinkList(title: ''),
+      home: const DrinkList(),
     );
   }
 }
 
 class DrinkList extends StatefulWidget {
-  const DrinkList({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const DrinkList({Key? key}) : super(key: key);
 
   @override
   State<DrinkList> createState() => _DrinkListState();
@@ -70,11 +59,6 @@ class _DrinkListState extends State<DrinkList> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
